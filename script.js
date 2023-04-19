0e0cbb8aa86b735ce338bcede68b81f9ce968983
relentlessslog <relentlessslog@gmail.com>
2023-04-18 21:09:32 -0600

    big ol phat update homie

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
+function getText(objectKey) {
+    const text = objectKey.text;
+    for (const value in text) {
+        document.querySelector(`.${value}__${objectKey.id}`).textContent = `${text[value]}`;
+    }
+}
+
+fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        const header = data.header;
+        getText(header);
+
+        data.sections.forEach((section) => getText(section));
+    })
+    .catch((error) => {
+        // Handle any errors
+        console.error(error);
+    });
+
+// Setting the width of the horizontal rule based on the computed width of the subtitle
+
 const subTitle = document.querySelectorAll(".subtitle");
 const hr = document.querySelectorAll("hr");
 
-subTitle.forEach( (e, i) => {
-   hr[i].style.width = e.clientWidth + "px";
+function hrSet(){
+    subTitle.forEach((e, i) => {
+        hr[i].style.width = e.clientWidth + "px";
+    });
+}
+
+window.addEventListener("resize", function () {
+    hrSet();
+});
+
+window.addEventListener("load", function(){
+    hrSet();
 })
